84cfb4feb1ac3a6f4e3e7e1bfd39ec438f8f93a7
kpilszak <klaudia.pilszak@gmail.com>
2025-05-28 09:19:32 -0400

    Clean the code to use one coach bean

--- src/main/java/com/kpilszak/employeesmanagement/rest/CoachController.java
+++ src/main/java/com/kpilszak/employeesmanagement/rest/CoachController.java
@@ -9,23 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CoachController {
     private Coach coach;
-    private Coach anotherCoach;
 
     @Autowired
-    public CoachController(
-            @Qualifier("cricketCoach") Coach coach,
-            @Qualifier("cricketCoach") Coach anotherCoach) {
+    public CoachController(@Qualifier("cricketCoach") Coach coach) {
         this.coach = coach;
-        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return coach.getDailyWorkout();
-    }
-
-    @GetMapping("/check")
-    public String check() {
-        return "Comparing beans: coach == anotherCoach" + (coach == anotherCoach);
     }
 }
