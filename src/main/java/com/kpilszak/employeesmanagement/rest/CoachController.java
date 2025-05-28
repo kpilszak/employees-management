f80472d64fc8bb575baab997213445f124906872
kpilszak <klaudia.pilszak@gmail.com>
2025-05-28 07:29:28 -0400

    Compare if singleton beans the same

--- src/main/java/com/kpilszak/employeesmanagement/rest/CoachController.java
+++ src/main/java/com/kpilszak/employeesmanagement/rest/CoachController.java
@@ -9,14 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CoachController {
     private Coach coach;
+    private Coach anotherCoach;
 
     @Autowired
-    public CoachController(@Qualifier("cricketCoach") Coach coach) {
+    public CoachController(
+            @Qualifier("cricketCoach") Coach coach,
+            @Qualifier("cricketCoach") Coach anotherCoach) {
         this.coach = coach;
+        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return coach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: coach == anotherCoach" + (coach == anotherCoach);
     }
 }
