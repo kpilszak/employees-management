ab908973b2a1148b98e9debfad0b53e9be9f2734
kpilszak <klaudia.pilszak@gmail.com>
2025-05-28 09:22:55 -0400

    Add custom post construct and pre destroy methods

--- src/main/java/com/kpilszak/employeesmanagement/common/CricketCoach.java
+++ src/main/java/com/kpilszak/employeesmanagement/common/CricketCoach.java
@@ -1,11 +1,23 @@
 package com.kpilszak.employeesmanagement.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements Coach {
     public CricketCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void doStartup() {
+        System.out.println("In doStartup(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doCleanup() {
+        System.out.println("In doCleanup(): " + getClass().getSimpleName());
     }
 
     @Override
