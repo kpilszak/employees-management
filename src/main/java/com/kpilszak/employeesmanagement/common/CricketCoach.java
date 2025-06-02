36cf7e6d7d9f57dd8e1e5d37c9ad37281f5a379f
kpilszak <klaudia.pilszak@gmail.com>
2025-06-02 09:36:18 -0400

    Clean custom post construct and pre destroy methods

--- src/main/java/com/kpilszak/employeesmanagement/common/CricketCoach.java
+++ src/main/java/com/kpilszak/employeesmanagement/common/CricketCoach.java
@@ -10,16 +10,6 @@ public class CricketCoach implements Coach {
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
 
-    @PostConstruct
-    public void doStartup() {
-        System.out.println("In doStartup(): " + getClass().getSimpleName());
-    }
-
-    @PreDestroy
-    public void doCleanup() {
-        System.out.println("In doCleanup(): " + getClass().getSimpleName());
-    }
-
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
