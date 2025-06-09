2c15904fa3b7ffa48f6d19ab38ba422be1550463
kpilszak <klaudia.pilszak@gmail.com>
2025-06-09 11:03:57 -0400

    Add db libs

--- src/main/java/com/kpilszak/employeesmanagement/EmployeesManagementApplication.java
+++ src/main/java/com/kpilszak/employeesmanagement/EmployeesManagementApplication.java
@@ -1,13 +1,22 @@
 package com.kpilszak.employeesmanagement;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeesManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeesManagementApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello from Runner");
+        };
     }
 
 }
