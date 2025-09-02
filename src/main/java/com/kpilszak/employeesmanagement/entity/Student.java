8d684e552561d1d272aa24775fee05337e897623
kpilszak <klaudia.pilszak@gmail.com>
2025-09-02 11:00:13 -0400

    Add getStudents endpoint

--- src/main/java/com/kpilszak/employeesmanagement/entity/Student.java
+++ src/main/java/com/kpilszak/employeesmanagement/entity/Student.java
@@ -16,15 +16,11 @@ public class Student {
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email")
-    private String email;
-
     public Student() {}
 
-    public Student(String firstName, String lastName, String email) {
+    public Student(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.email = email;
     }
 
     public int getId() {
@@ -51,21 +47,12 @@ public class Student {
         this.lastName = lastName;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Override
     public String toString() {
         return "Student{" +
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
                 '}';
     }
 }
