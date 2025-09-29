91daa66de52da1e3d1335689da8c488190dab710
kpilszak <klaudia.pilszak@gmail.com>
2025-09-29 10:31:57 -0400

    Expose findAll for Employees

---
+++ src/main/java/com/kpilszak/employeesmanagement/rest/EmployeeRestController.java
@@ -0,0 +1,24 @@
+package com.kpilszak.employeesmanagement.rest;
+
+import com.kpilszak.employeesmanagement.dao.EmployeeDAO;
+import com.kpilszak.employeesmanagement.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
